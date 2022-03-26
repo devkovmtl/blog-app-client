8c4173b473e96064b004c844046d3e7785eb21f9
Mikael <dev.kov.mtl@gmail.com>
2022-03-26 18:57:05 -0400

    Post Comment to backend

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,4 +1,6 @@
-const { useState } = require('react');
+import { useState } from 'react';
+import axios from 'axios';
+import { SERVER_URL } from '../constants';
 
 const CommentForm = ({ postId }) => {
   const [username, setUsername] = useState('');
@@ -6,28 +8,31 @@ const CommentForm = ({ postId }) => {
   const [errors, setErrors] = useState({
     username: '',
     content: '',
+    submitError: '',
   });
 
   const onUsernameChange = (e) => {
     let newErrors = {
       username: '',
+      submitError: '',
     };
     setErrors((prevState) => ({ ...prevState, ...newErrors }));
-    setUsername(e.target.value.trim());
+    setUsername(e.target.value);
   };
 
   const onContentChange = (e) => {
     let newErrors = {
       content: '',
+      submitError: '',
     };
     setErrors((prevState) => ({ ...prevState, ...newErrors }));
-    setContent(e.target.value.trim());
+    setContent(e.target.value);
   };
 
-  const submitForm = (e) => {
+  const submitForm = async (e) => {
     e.preventDefault();
     let updatedErrors = {};
-    if (!username && !content) {
+    if (!username.trim() && !content.trim()) {
       updatedErrors = {
         username: 'Username is required with at least 1 character',
         content: 'Comment is required with at least 1 character',
@@ -38,7 +43,7 @@ const CommentForm = ({ postId }) => {
       }));
       return;
     }
-    if (!username) {
+    if (!username.trim()) {
       updatedErrors = {
         username: 'Username is required with at least 1 character',
       };
@@ -48,7 +53,7 @@ const CommentForm = ({ postId }) => {
       }));
       return;
     }
-    if (!content) {
+    if (!content.trim()) {
       updatedErrors = {
         content: 'Comment is required with at least 1 character',
       };
@@ -60,67 +65,90 @@ const CommentForm = ({ postId }) => {
     }
 
     // TODO send data to backend
-    const newComment = { username, content };
+    const newComment = { username: username.trim(), content: content.trim() };
+
+    try {
+      const response = await axios.post(
+        SERVER_URL + `comments/${postId}`,
+        newComment
+      );
+      if (response.success) {
+        setUsername('');
+        setContent('');
+      }
+    } catch (error) {
+      updatedErrors = {
+        submitError: 'Something went wrong. Try again.',
+      };
+      setErrors({ ...errors, ...updatedErrors });
+    }
   };
 
   return (
-    <form className='form' onSubmit={submitForm} noValidate>
-      <div className='form__group'>
-        <label
-          htmlFor='username'
-          className={`input-group__label ${
-            errors.username ? 'input-group__label--invalid' : ''
-          }`}
-        >
-          <span style={{ color: '#ff0000' }}>*</span> Username
-        </label>
-        <input
-          type='text'
-          id='username'
-          name='username'
-          className={`input-group__input ${
-            errors.username ? 'input-group__input--invalid' : ''
-          }`}
-          required
-          value={username}
-          onChange={onUsernameChange}
-        />
-        {errors.username && (
-          <div className='input-group--invalid-feedback'>
-            <p>{errors.username}</p>
-          </div>
-        )}
-      </div>
-      <div className='form__group'>
-        <label
-          htmlFor='content'
-          className={`input-group__label ${
-            errors.content ? 'input-group__label--invalid' : ''
-          }`}
-        >
-          <span style={{ color: '#ff0000' }}>*</span> Comment
-        </label>
-        <textarea
-          name='content'
-          id='content'
-          cols='30'
-          rows='10'
-          className={`input-group__textarea ${
-            errors.content ? 'input-group__textarea--invalid' : ''
-          }`}
-          placeholder='Enter your comment'
-          required
-          value={content}
-          onChange={onContentChange}
-        ></textarea>
-        {errors.content && (
-          <div className='input-group--invalid-feedback'>
-            <p>{errors.content}</p>
-          </div>
-        )}
-      </div>
-      <input type='submit' value='Submit' className='input__submit' />
-    </form>
+    <>
+      <form className='form' onSubmit={submitForm} noValidate>
+        <div className='form__group'>
+          <label
+            htmlFor='username'
+            className={`input-group__label ${
+              errors.username ? 'input-group__label--invalid' : ''
+            }`}
+          >
+            <span style={{ color: '#ff0000' }}>*</span> Username
+          </label>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            className={`input-group__input ${
+              errors.username ? 'input-group__input--invalid' : ''
+            }`}
+            required
+            value={username}
+            onChange={onUsernameChange}
+          />
+          {errors.username && (
+            <div className='input-group--invalid-feedback'>
+              <p>{errors.username}</p>
+            </div>
+          )}
+        </div>
+        <div className='form__group'>
+          <label
+            htmlFor='content'
+            className={`input-group__label ${
+              errors.content ? 'input-group__label--invalid' : ''
+            }`}
+          >
+            <span style={{ color: '#ff0000' }}>*</span> Comment
+          </label>
+          <textarea
+            name='content'
+            id='content'
+            cols='30'
+            rows='10'
+            className={`input-group__textarea ${
+              errors.content ? 'input-group__textarea--invalid' : ''
+            }`}
+            placeholder='Enter your comment'
+            required
+            value={content}
+            onChange={onContentChange}
+          ></textarea>
+          {errors.content && (
+            <div className='input-group--invalid-feedback'>
+              <p>{errors.content}</p>
+            </div>
+          )}
+        </div>
+        <input type='submit' value='Submit' className='input__submit' />
+      </form>
+      {errors.submitError ? (
+        <p style={{ textAlign: 'center', fontSize: '14px' }}>
+          {errors.submitError}
+        </p>
+      ) : null}
+    </>
   );
 };
 
