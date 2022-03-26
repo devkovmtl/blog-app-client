08f1a241a4ec0c9441cfb97969449228e22b15ef
Mikael <dev.kov.mtl@gmail.com>
2022-03-26 15:39:52 -0400

    installed axios and fetch posts from backend

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -1,65 +1,69 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-const posts = [
-  {
-    _id: Math.floor(Math.random() * 99999),
-    title: 'Post Title 1',
-    content: 'Post Content 1 ...',
-    createdAt: Date.now().toLocaleString(),
-    author: {
-      username: 'AuthorUsername',
-    },
-  },
-  {
-    _id: Math.floor(Math.random() * 99999),
-    title: 'Post Title 2',
-    content: 'Post Content 2 ...',
-    createdAt: Date.now().toLocaleString(),
-    author: {
-      username: 'AuthorUsername',
-    },
-  },
-  {
-    _id: Math.floor(Math.random() * 99999),
-    title: 'Post Title 3 ',
-    content: 'Post Content 3 ...',
-    createdAt: Date.now().toLocaleString(),
-    author: {
-      username: 'AuthorUsername',
-    },
-  },
-];
+import axios from 'axios';
 
 const PostListItem = () => {
+  const [isLoading, setIsLoading] = useState();
+  const [posts, setPosts] = useState([]);
+  const [errors, setErrors] = useState();
+
   let navigate = useNavigate();
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    setIsLoading(true);
+    const fetchPost = async () => {
+      try {
+        const { data } = await axios.get(`http://localhost:8080`);
+        setIsLoading(false);
+        if (data.success) {
+          console.log(data.posts.length);
+          setPosts([...data.posts]);
+        } else {
+          setErrors('Sorry, an error occurred. Try again.');
+        }
+      } catch (error) {
+        setIsLoading(false);
+        setErrors('Sorry, an error occurred. Try again.');
+      }
+    };
+    fetchPost();
   }, []);
 
-  return (
-    <div>
-      {posts.map(({ _id, title, content, author, createdAt }) => (
-        <div className='post__list__item' key={_id}>
-          <div
-            className='post__item__header'
-            onClick={() => navigate(`${_id}`)}
-          >
-            <h1 className='post__item__title'>{title}</h1>
-            <p className='post__item__subtitle'>
-              Posted by <strong>{author.username}</strong>, on {createdAt}
-            </p>
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (errors) {
+    content = <h3>{errors}</h3>;
+  } else {
+    content =
+      posts && posts.length ? (
+        posts.map(({ _id, title, content, author, createdAt }) => (
+          <div className='post__list__item' key={_id}>
+            <div
+              className='post__item__header'
+              onClick={() => navigate(`${_id}`)}
+            >
+              <h1 className='post__item__title'>{title}</h1>
+              <p className='post__item__subtitle'>
+                Posted by <strong>{author.username}</strong>, on {createdAt}
+              </p>
+            </div>
+            <div className='post__item__body'>
+              <p>{content}</p>
+            </div>
+            <div className='post__item__footer'>
+              <p>21 Comments</p>
+            </div>
           </div>
-          <div className='post__item__body'>
-            <p>{content}</p>
-          </div>
-          <div className='post__item__footer'>
-            <p>21 Comments</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+        ))
+      ) : (
+        <h3>No post yet.</h3>
+      );
+  }
+
+  return <div>{content}</div>;
 };
 
 export default PostListItem;
